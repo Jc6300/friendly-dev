9ab7b3feea93eec5ccac30a52b24f23d2edb5314
Josh <joshcotton93@gmail.com>
2025-07-30 13:46:54 -0500

    hero

--- app/routes/about/index.tsx
+++ app/routes/about/index.tsx
@@ -1,7 +1,7 @@
 const AboutPage = () => {
   return (
     <section>
-      <h1 className="text-3xl font-bold text-white mg-2">Hey, I'm Josh 👋🏽</h1>
+      <h1 className="text-3xl font-bold text-white mg-2">Hey, I'm Josh 👋🏼</h1>
     </section>
   );
 };
