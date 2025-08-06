30399cd4df910f7bfb74dd2cfcb7dd69b32042a0
Josh <joshcotton93@gmail.com>
2025-08-05 22:17:49 -0500

    added blog data

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,22 @@
-const blogPage = () => {
+import type { Route } from "./+types";
+import { Link } from "react-router";
+import type { PostMeta } from "~/types";
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const res = await fetch(url.href);
+
+  if (!res.ok) throw new Error("Failed to fetch data");
+
+  const data = await res.json();
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
+  console.log(posts);
+
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">📝 Blog</h2>
@@ -6,4 +24,4 @@ const blogPage = () => {
   );
 };
 
-export default blogPage;
+export default BlogPage;
