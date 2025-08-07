928b2f9c63fdf5a2ca0a6f8c886b8360754ef11a
Josh <joshcotton93@gmail.com>
2025-08-06 20:52:22 -0500

    created strapi types

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types/index";
-import type { Project } from "~/types";
+import type { Project, StrapiProject, StrapiResponse } from "~/types";
 import Pagination from "~/components/Pagination";
 import { AnimatePresence, motion } from "framer-motion";
 
@@ -11,7 +11,7 @@ export async function loader({
   const res = await fetch(
     `${import.meta.env.VITE_API_URL}/projects?populate=*`
   );
-  const json = await res.json();
+  const json: StrapiResponse<StrapiProject> = await res.json();
 
   const projects = json.data.map((item) => ({
     id: item.id,
